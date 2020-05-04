1950e10f9f0ebdfb8791a3d482c09d01dcb3c2d8
Mykola Nesvietaiev <nesvietaiev@gmail.com>
2020-05-05 00:52:46 +0300

    Сделана адаптация под размеры экранов

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 
 $(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
+  $('.header-navbar__hamburger').on('click', function(){
     $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
   });
 
 });
